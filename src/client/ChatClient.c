ad6479b85bc339619ba59930da9e3b7dd37d8f5c
Samuel Butler <43868419+AsterCoyote@users.noreply.github.com>
2025-11-07 16:02:36 -0700

    Renamed some stuff and got the client h file started

---
+++ src/client/ChatClient.c
@@ -0,0 +1,7 @@
+#include "client.h"
+
+int main(int argc, char const *argv[])
+{
+    printf( "-- Simple Chat --" );
+    return 0;
+}
---
+++ src/client/ChatClient.h
@@ -0,0 +1,39 @@
+// Include inportant libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// Networking libraries
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+// Define some macro constants
+#define MAX_TEXT_CHARS 64
+#define MAX_USERNAME_CHARS 32
+
+// For specific int sizes
+#include <stdint.h>
+
+// Defines the type of message that is being sent
+enum MessageTypeCode
+{
+    JOIN,
+    LEAVE,
+    NOTE,
+    SHUTDOWN,
+    SHUTDOWN_ALL,
+    DIRECT_MESSAGE
+};
+
+
+// Struct for sent message
+struct MessageStruct
+{
+    enum MessageTypeCode type;
+    char sender[ MAX_USERNAME_CHARS ];
+    char receiver[ MAX_USERNAME_CHARS ];
+    char text[ MAX_TEXT_CHARS ];
+} Message;
+
+// Function prototype definitions
