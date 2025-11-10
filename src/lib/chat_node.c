f33a260a715fcc1d2303ee3a0bfdc339728e9150
kbchip <charlieshaskins@gmail.com>
2025-11-09 20:28:02 -0700

    Add function to remove chat node by username, and fix pointer type on chatter_node_compare()

--- src/lib/chat_node.c
+++ src/lib/chat_node.c
@@ -4,8 +4,10 @@ ChatterNode* create_chatter_node();
 
 ChatterNodeList* create_chatter_node_list();
 
-ChatterNodeList* add_chatter_node(ChatterNodeList* list);
+ChatterNodeList* add_chatter_node(ChatterNodeList* list, ChatterNode* node);
 
-ChatterNodeList* remove_chatter_node(ChatterNodeList* list);
+ChatterNodeList* remove_chatter_node(ChatterNodeList* list, ChatterNode* node);
 
-bool chatter_node_compare(ChatterNode left, ChatterNode right);
+ChatterNodeList* remove_chatter_by_username(ChatterNodeList* list, char *username);
+
+bool chatter_node_compare(ChatterNode *left, ChatterNode *right);
